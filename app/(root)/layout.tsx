780d5ecae67ead4b96488a49532f1f3cb03c25ee
Briell Quintana <brielluh@gmail.com>
2025-06-02 06:48:29 -0400

    refactor: update layout structure and improve CarouselCard image source format

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -4,7 +4,10 @@ import { ReactNode } from "react";
 
 const Layout = ({ children }: { children: ReactNode }) => {
   return (
-    <div className="flex min-h-screen flex-col transition select-none">
+    <div
+      aria-hidden
+      className="grid min-h-screen grid-rows-[auto_1fr_auto] select-none"
+    >
       <Navbar />
       {children}
       <Footer />
