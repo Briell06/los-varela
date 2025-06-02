0e5d8e4c1fb5a91a0120171aa41555afa69e5380
Briell06 <brielluh@gmail.com>
2025-06-02 11:57:37 -0400

    Actualizar layout.tsx

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -6,7 +6,9 @@ const Layout = ({ children }: { children: ReactNode }) => {
   return (
     <div className="grid min-h-screen grid-rows-[auto_1fr_auto] grid-cols-1 select-none">
       <Navbar />
+       <main role="main" className="container mx-auto" >
         {children}
+       </main>
       <Footer />
     </div>
   );
