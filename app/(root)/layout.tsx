9707d2179d47387cd4d083261c023de675299c09
Briell Quintana <brielluh@gmail.com>
2025-06-02 06:57:23 -0400

    refactor: remove unnecessary aria-hidden attribute from layout component

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -4,10 +4,7 @@ import { ReactNode } from "react";
 
 const Layout = ({ children }: { children: ReactNode }) => {
   return (
-    <div
-      aria-hidden
-      className="grid min-h-screen grid-rows-[auto_1fr_auto] select-none"
-    >
+    <div className="grid min-h-screen grid-rows-[auto_1fr_auto] select-none">
       <Navbar />
       <main className="container mx-auto">{children}</main>
       <Footer />
