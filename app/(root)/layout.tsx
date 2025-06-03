93e80978bb25ec840f6e6bb64fad1be3a86014ca
Briell Quintana <brielluh@gmail.com>
2025-06-03 02:36:05 -0400

    Add aria-hidden attribute to main element for improved accessibility

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -6,7 +6,7 @@ const Layout = ({ children }: { children: ReactNode }) => {
   return (
     <div className="flex min-h-screen flex-col select-none">
       <Navbar />
-      <main role="main" className="container mx-auto flex-1">
+      <main role="main" aria-hidden className="container mx-auto flex-1">
         {children}
       </main>
       <Footer />
